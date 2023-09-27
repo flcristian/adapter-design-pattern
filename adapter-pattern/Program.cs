3a0dcf2c6e71636c1643f1ddd309e10fd79a1f58
flcristian <qflorescucristian@gmail.com>
2023-09-27 21:08:23 +0300

    Adapter pattern

---
+++ adapter-pattern/Program.cs
@@ -0,0 +1,10 @@
+﻿using adapter_pattern;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Target target = new Adapter();
+        target.Request();
+    }
+}
